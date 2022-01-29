245a70b24eaf9453c9b71e8d3cf21dbf279f8fb9
dide <justgamegeorge@gmail.com>
2022-01-29 14:26:54 +0500

    запушили программ

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("23 Hello World!");
+        }
+    }
+}
